c65a31f83ba4ca79dd7d19a9d3fb0c3847143e83
Emir Kaya <emir.kaya@eaegroup.com>
2025-09-24 17:39:39 +0300

    Proje dosyası ekle.

---
+++ Stock.API/Program.cs
@@ -0,0 +1,20 @@
+using MassTransit;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMassTransit(configurator =>
+{
+
+    configurator.UsingRabbitMq((context, _configure) =>
+    {
+        _configure.Host(builder.Configuration["RabbitMQ"]);
+    });
+});
+
+
+
+var app = builder.Build();
+
+
+
