5ba28f18366156b66b380f37ebc472abe6ffb90d
emir kaya <emir kaya@DESKTOP-CM0RVOG>
2025-09-28 23:05:25 +0300

    feat: implement saga orchestration pattern with order processing workflow

--- Stock.API/Program.cs
+++ Stock.API/Program.cs
@@ -1,14 +1,24 @@
 using MassTransit;
-
+using Stock.API.Services;
+using MongoDB.Driver;
+using Stock.API.Consumers;
+using Shared.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSingleton<MongoDbService>();
+
 builder.Services.AddMassTransit(configurator =>
 {
-
+    configurator.AddConsumer<OrderCreatedEventConsumer>();
+    configurator.AddConsumer<StockRollbackMessageConsumer>();
     configurator.UsingRabbitMq((context, _configure) =>
     {
         _configure.Host(builder.Configuration["RabbitMQ"]);
+
+        _configure.ReceiveEndpoint(RabbitMQSettings.Stock_OrderCreatedEventQueue, e => e.ConfigureConsumer<OrderCreatedEventConsumer>(context));
+
+        _configure.ReceiveEndpoint(RabbitMQSettings.Stock_RollbackMessageQueue, e => e.ConfigureConsumer<StockRollbackMessageConsumer>(context));
     });
 });
 
@@ -17,4 +27,35 @@ builder.Services.AddMassTransit(configurator =>
 var app = builder.Build();
 
 
+using var scope = builder.Services.BuildServiceProvider().CreateScope();
+var mongoDbService = scope.ServiceProvider.GetRequiredService<MongoDbService>();
+if (!await (await mongoDbService.GetCollection<Stock.API.Models.Stock>().FindAsync(x => true)).AnyAsync())
+{
+    mongoDbService.GetCollection<Stock.API.Models.Stock>().InsertOne(new()
+    {
+        ProductId = 1,
+        Count = 200
+    });
+    mongoDbService.GetCollection<Stock.API.Models.Stock>().InsertOne(new()
+    {
+        ProductId = 2,
+        Count = 300
+    });
+    mongoDbService.GetCollection<Stock.API.Models.Stock>().InsertOne(new()
+    {
+        ProductId = 3,
+        Count = 50
+    });
+    mongoDbService.GetCollection<Stock.API.Models.Stock>().InsertOne(new()
+    {
+        ProductId = 4,
+        Count = 10
+    });
+    mongoDbService.GetCollection<Stock.API.Models.Stock>().InsertOne(new()
+    {
+        ProductId = 5,
+        Count = 60
+    });
+}
 
+app.Run();
